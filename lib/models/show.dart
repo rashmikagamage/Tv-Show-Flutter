dcd4e38fbd2b7c61add3d115feee9cd9da7e84a8
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2021-05-26 18:45:12 +0530

    Home screen slider created

---
+++ lib/models/show.dart
@@ -0,0 +1,19 @@
+class Show{
+  String name;
+  String imageUrl;
+  String showTime;
+  String day;
+  String channel;
+  String description;
+  double rating;
+
+  Show({
+    this.name,
+    this.imageUrl,
+    this.showTime,
+    this.day,
+    this.channel,
+    this.description,
+    this.rating
+  });
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tvshowsapp/services/firestore_services.dart';
+import 'package:tvshowsapp/services/dataProvider.dart';
 
 class HomeScreeen extends StatefulWidget {
   @override
@@ -7,14 +8,77 @@ class HomeScreeen extends StatefulWidget {
 }
 
 class _HomeScreeenState extends State<HomeScreeen> {
-
   PageController _pageController;
-
   @override
-    void initState() {
-      super.initState();
-      _pageController = PageController(initialPage: 1,viewportFraction: 0.8);
-    }
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 1, viewportFraction: 0.8);
+  }
+
+  _showSelector(int index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, Widget widget) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = _pageController.page - index;
+          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 270.0,
+            width: Curves.easeInOut.transform(value) * 400.0,
+            child: widget,
+          ),
+        );
+      },
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(0.0, 4.0),
+                      blurRadius: 10.0)
+                ],
+              ),
+              child: Center(
+                child: Hero(
+                  tag: showList[index].imageUrl,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image(
+                      image: NetworkImage(showList[index].imageUrl),
+                      height: 220.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 30.0,
+            bottom: 40.0,
+            child: Container(
+              width: 250.0,
+              child: Text(
+                showList[index].name.toUpperCase(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,21 +87,19 @@ class _HomeScreeenState extends State<HomeScreeen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: IconButton(
-          padding: EdgeInsets.only(left: 30.0),
-          onPressed: ()=>print('Menu'),
-          icon:Icon(Icons.menu),
-          iconSize: 30.0,
-          color:Colors.black
-        ),
+            padding: EdgeInsets.only(left: 30.0),
+            onPressed: () => print('Menu'),
+            icon: Icon(Icons.menu),
+            iconSize: 30.0,
+            color: Colors.black),
         title: Center(
           child: Text(
             'TV-Shows'.toUpperCase(),
             style: Theme.of(context).textTheme.caption.copyWith(
-                color: Colors.black45,
+                color: Colors.red[600],
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                fontFamily: 'muli'
-            ),
+                fontFamily: 'muli'),
           ),
         ),
         actions: [
@@ -45,6 +107,21 @@ class _HomeScreeenState extends State<HomeScreeen> {
             margin: EdgeInsets.only(right: 15),
             child: CircleAvatar(
               backgroundImage: AssetImage('assets/images/profPic.png'),
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 280.0,
+            width: double.infinity,
+            child: PageView.builder(
+              controller: _pageController,
+              itemBuilder: (BuildContext context, int index) {
+                return _showSelector(index);
+              },
+              itemCount: showList.length,
             ),
           )
         ],
