498e3044c762046ea65c86ef2a4a017b1b4235d9
Niroshan-Krishnamoorthy <niro.dev97@gmail.com>
2021-06-01 08:00:38 +0530

    side bar completed

--- lib/widgets/side_bar.dart
+++ lib/widgets/side_bar.dart
@@ -5,10 +5,45 @@ class SideBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
+        padding: EdgeInsets.zero,
         children: [
           UserAccountsDrawerHeader(
-              accountName: Text('Niroshan'),
-              accountEmail: Text('test@test.com')),
+            accountName: Text('Niroshan'),
+            accountEmail: Text('test@test.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.asset(
+                  'assets/images/profPic.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/bg.jpg'), fit: BoxFit.cover)),
+          ),
+          ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Favorites'),
+              onTap: () => print('fav clicled!')),
+          ListTile(
+              leading: Icon(Icons.tv),
+              title: Text('Channels'),
+              onTap: () => print('channels clicled!')),
+          Divider(),
+          ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () => print('settings clicled!')),
+          ListTile(
+              leading: Icon(Icons.info),
+              title: Text('About us'),
+              onTap: () => print('about us clicled!')),
+          Divider(),
+          ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Sign Out'),
+              onTap: () => print('sign out clicled!')),
         ],
       ),
     );
