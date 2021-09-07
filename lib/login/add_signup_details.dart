9d88036067e3c705da206ebf40f99c43285f7df4
Gimhani Ashcharya Hemachandra <52479595+GimhaniAsh@users.noreply.github.com>
2021-09-07 05:42:24 +0530

    Update the implementation

--- lib/login/add_signup_details.dart
+++ lib/login/add_signup_details.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+
 Future<void> userSetup(String email, String contact, String imgUrl) async {
   CollectionReference users = FirebaseFirestore.instance.collection('Users');
   FirebaseAuth auth = FirebaseAuth.instance;
